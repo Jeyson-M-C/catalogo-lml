3a9e087211e0c41085a2301c08b5bc29f3294562
Jeyson-M-C <jeyson.moran@outlook.com>
2025-02-24 21:38:29 -0600

    catalogo-lml-12

--- run.py
+++ run.py
@@ -1,44 +1,18 @@
 import os
 import uvicorn
-import signal
 
-class TimeoutError(Exception): 
-    pass 
+# Configuración del entorno
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+HOST = "localhost" if DEBUG else "0.0.0.0"  # Ajusta el host según el entorno
+PORT = int(os.getenv("PORT", "8000"))  # Asegura que el puerto sea un entero
 
-def input_with_timeout(prompt, timeout):
-    def alarm_handler(signum, frame):
-        raise TimeoutError
+# Imprimir la configuración
+environment = "LOCAL" if DEBUG else "PRODUCCIÓN"
+print(f"Corriendo en modo: {environment}")
+print(f"Escuchando en host: {HOST}, puerto: {PORT}")
 
-    signal.signal(signal.SIGALRM, alarm_handler)
-    signal.alarm(timeout)
-
-    try:
-        return input(prompt)
-    finally:
-        signal.alarm(0)
-
-def main():
-    try:
-        response = input_with_timeout("¿Deseas ejecutar en modo local? (1 para local, cualquier otra tecla para producción): ", 10)
-        return response == "1"
-    except TimeoutError:
-        print("\nNo se recibió respuesta. Ejecutando en modo PRODUCCIÓN por defecto.")
-        return False
-
-# Pregunta al usuario por el entorno
+# Ejecutar Uvicorn
 if __name__ == "__main__":
-    DEBUG = main()
-
-    # Configuración del entorno
-    HOST = "localhost" if DEBUG else "0.0.0.0"  # Ajusta el host según el entorno
-    PORT = int(os.getenv("PORT", "8000"))  # Asegura que el puerto sea un entero
-
-    # Imprimir la configuración
-    environment = "LOCAL" if DEBUG else "PRODUCCIÓN"
-    print(f"Corriendo en modo: {environment}")
-    print(f"Escuchando en host: {HOST}, puerto: {PORT}")
-
-    # Ejecutar Uvicorn
     uvicorn.run(
         "main:app",  # Importante: usa el nombre del archivo y la instancia de la app
         host=HOST,
